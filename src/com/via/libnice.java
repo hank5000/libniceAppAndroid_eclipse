c647224c1e67ceac2ec0719e8466ff4883b0d81f
HankWu <HankWu@via.com.tw>
2016-01-25 18:38:16 +0800

    1. Add Agent Handle such that can support multi class create

--- src/com/via/libnice.java
+++ src/com/via/libnice.java
@@ -2,8 +2,6 @@ package com.via;
 import java.nio.ByteBuffer;
 
 import android.util.Log;
-
-import com.example.libnice.MainActivity;
 import com.gstreamer.GStreamer;
 
 public class libnice {
@@ -17,44 +15,40 @@ public class libnice {
 	
 	String[] mStreamName = new String[MAX_STREAM];
 	String[][] mComponentName = new String[MAX_STREAM][MAX_COMPONENT];
-	
+	long agentCtxHandle = 0;
+
 	private native int initNative();
-	private native int createAgentNative(int useReliable);
-	private native int setStunAddressNative(String stun_ip,int stun_port);
-	private native int setControllingModeNative(int controllingMode);
-	private native int /*stream id*/ addStreamNative(String streamName, int numberOfComponent); // return stream id which is signed by libnice
-	private native String /*sdp which is encoded by base 64*/ getLocalSdpNative(int stream_id);
-	private native int setRemoteSdpNative(String jremoteSdp,long Size);
-	private native int sendMsgNative(String data,int streamId,int compId);
-	private native int sendDataNative(byte[] data,int len ,int streamId,int compId);
-	private native int sendDataDirectNative(ByteBuffer data, int len ,int streamId,int compId);
-	private native int setDirectBufferIndexNative(ByteBuffer data, int index);
-	private native int sendDataDirectByIndexNative(ByteBuffer data, int len ,int index,int streamId,int compId);
-	private native int restartStreamNative(int streamId);
+	private native long /*agent handle*/ createAgentNative(int useReliable);
+	private native int setStunAddressNative(long agentHandle,String stun_ip,int stun_port);
+	private native int setControllingModeNative(long agentHandle,int controllingMode);
+	private native int /*stream id*/ addStreamNative(long agentHandle,String streamName, int numberOfComponent); // return stream id which is signed by libnice
+	private native String /*sdp which is encoded by base 64*/ getLocalSdpNative(long agentHandle,int stream_id);
+	private native int setRemoteSdpNative(long agentHandle,String jremoteSdp,long Size);
+	private native int sendMsgNative(long agentHandle,String data,int streamId,int compId);
+	private native int sendDataNative(long agentHandle,byte[] data,int len ,int streamId,int compId);
+	private native int sendDataDirectNative(long agentHandle,ByteBuffer data, int len ,int streamId,int compId);
+	private native int setDirectBufferIndexNative(long agentHandle,ByteBuffer data, int index);
+	private native int sendDataDirectByIndexNative(long agentHandle,ByteBuffer data, int len ,int index,int streamId,int compId);
+	private native int restartStreamNative(long agentHandle,int streamId);
 	
 	public int restartStream(int streamId) {
-		return restartStreamNative(streamId);
+		return restartStreamNative(agentCtxHandle,streamId);
 	}
 
 	public int sendDataDirectByIndex(ByteBuffer data,int len,int index,int streamId,int CompId) {
-		return sendDataDirectByIndexNative(data,len,index,streamId,CompId);
+		return sendDataDirectByIndexNative(agentCtxHandle,data,len,index,streamId,CompId);
 	}
 	
 	public int setDirectBufferIndex(ByteBuffer data,int index) {
-		return setDirectBufferIndexNative(data,index);
+		return setDirectBufferIndexNative(agentCtxHandle,data,index);
 	}
 	
 	
 	private native int mainLoopStart();
 	// register callback function for stream[streamId],component[compId]
-	private native void registerReceiveCallbackNative(libnice.ReceiveCallback recv_cb_obj,int streamId,int compId);
-	private native void registerStateObserverNative(libnice.StateObserver obs);
+	private native void registerReceiveCallbackNative(long agentHandle,libnice.ReceiveCallback recv_cb_obj,int streamId,int compId);
+	private native void registerStateObserverNative(long agentHandle,libnice.StateObserver obs);
 	
-	static MainActivity act;
-
-	public void setAct(MainActivity a) {
-		act = a;
-	}
 
 	private static int a = 0;
 	Thread mainLoopThread = new Thread(new Runnable(){
@@ -73,68 +67,54 @@ public class libnice {
 	}
 
 	public int createAgent(int useReliable) {
-		return createAgentNative(useReliable);
+		agentCtxHandle = createAgentNative(useReliable);
+		return 1;
 	}
 
 	public int setStunAddress(String stun_ip, int stun_port) {
-		return setStunAddressNative( stun_ip, stun_port);
+		return setStunAddressNative(agentCtxHandle,stun_ip, stun_port);
 	}
 
 	public int setControllingMode(int controllingMode) {
-		return setControllingModeNative(controllingMode);
+		return setControllingModeNative(agentCtxHandle,controllingMode);
 	}
 
 	public int addStream(String streamName, int numberOfComponent) {
-		return addStreamNative(streamName, numberOfComponent);
+		return addStreamNative(agentCtxHandle,streamName, numberOfComponent);
 	}
 
 	public String getLocalSdp(int streamId) {
-		return getLocalSdpNative(streamId);
+		return getLocalSdpNative(agentCtxHandle,streamId);
 	}
 
 	public void setRemoteSdp(String remoteSdp) {
-		setRemoteSdpNative(remoteSdp,remoteSdp.length());
+		setRemoteSdpNative(agentCtxHandle,remoteSdp,remoteSdp.length());
 	}
 
 	public void sendData(byte[] buf,int len,int streamId ,int compId) {
-		sendDataNative(buf,len,streamId,compId);
+		sendDataNative(agentCtxHandle,buf,len,streamId,compId);
 	}
 	
 	public void sendDataDirect(ByteBuffer buf, int len,int streamId, int compId) {
-		sendDataDirectNative(buf,len,streamId,compId);
+		sendDataDirectNative(agentCtxHandle,buf,len,streamId,compId);
 	}
 	
 	
 	public void sendMsg(String msg, int streamId,int compId) {
-		sendMsgNative(msg,streamId,compId);
+		sendMsgNative(agentCtxHandle,msg,streamId,compId);
 	}
 
-	public void jniCallBackMsg(String msg) {
-		Log.d("libnice-cb ",msg);
-	}
-	
-	public void jniCallBackInt(int i) {
-		Log.d("libnice-cb ",i+"");
-	}
-	
-	public static void jniCallBackMsgStatic(byte[] msg) {
-		Log.d("libnice-cb-static", new String(msg));
-		act.AddTextToChat("From:"+new String(msg));
-	}
-	
 	public void registerReceiveCallback(libnice.ReceiveCallback obs,int streamId ,int compId) {
-		this.registerReceiveCallbackNative(obs,streamId,compId);
+		this.registerReceiveCallbackNative(agentCtxHandle,obs,streamId,compId);
 	}
 	
 	public void registerStateObserver(libnice.StateObserver stateObserver) {
-		this.registerStateObserverNative(stateObserver);
+		this.registerStateObserverNative(agentCtxHandle,stateObserver);
 	}
 		
 	public interface ReceiveObserver{
 		void obCallback(byte[] msg);
 	}
-	
-	
 	
 	public interface StateObserver {
 		String[] STATE_TABLE = {"disconnected", "gathering", "connecting",
